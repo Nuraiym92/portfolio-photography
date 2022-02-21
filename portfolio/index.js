ab8d79e8e3b549559923f080ca87e1f0fc745d68
Nuraiym <nuraiym.zhyrgalbekova.kk@gmail.com>
2022-02-21 11:45:22 +0600

    portfolio

---
+++ portfolio/index.js
@@ -0,0 +1,16 @@
+// console.log(" Вёрстка валидная-5баллов,header,main, footer-2,шесть элементов section (по количеству секций)-2балл,только один заголовок h1-2балл,блок header-6баллов,секция hero,skills,portfolio,contacts- по 6 баллов,footer-6 баллов,Требования к css -10 баллов,Интерактивность-15.Общий балл_97");
+
+// const burger=document.querySelector(".burger"),
+
+//           nav=document.querySelector(".menu");
+
+//            burger.addEventListener('click', () => {
+//            burger.classList.toggle("toggle")
+//            nav.classList.toggle("nav-change")
+//           });
+
+//        document.querySelectorAll('.nav-link').forEach(item => {
+//             item.addEventListener('click', event => {
+//               nav.classList.remove("nav-change")
+//             })
+//           })
